14414b77962dfc652583dcd7f49ab3d198b8b00d
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-16 19:45:50 -0400

    Create spreadsheet.py

---
+++ spreadsheet.py
@@ -0,0 +1,65 @@
+# Create string of lettered columns to load
+col_string = "AD, AW:BA"
+
+# Load data with skiprows and usecols set
+survey_responses = pd.read_excel("fcc_survey_headers.xlsx", 
+                                 skiprows=2, 
+                                 usecols=col_string)
+
+# View the names of the columns selected
+print(survey_responses.columns)
+
+
+""" multiple sheets """
+# Create df from second worksheet by referencing its name
+responses_2017 = pd.read_excel("fcc_survey.xlsx",
+                               sheet_name='2017')
+
+# Graph where people would like to get a developer job
+job_prefs = responses_2017.groupby("JobPref").JobPref.count()
+job_prefs.plot.barh()
+plt.show()
+
+# Load both the 2016 and 2017 sheets by name or sheet_name=None
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name=['2016','2017'])
+
+# View the data type of all_survey_data
+print(type(all_survey_data))
+
+# Load all sheets in the Excel file
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name=[0, '2017'])
+
+# View the sheet names in all_survey_data
+print(all_survey_data.keys())
+
+# Load all sheets in the Excel file
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name=None)
+
+# View the sheet names in all_survey_data
+print(all_survey_data.keys())
+
+
+
+""" iterating through the sheets
+ All sheets have been read into the ordered dictionary responses, 
+ where sheet names are keys and data frames are values, so you can get data frames with the values() method.
+"""
+
+# Create an empty data frame
+all_responses = pd.DataFrame()
+
+# Set up for loop to iterate through values in responses
+for df in responses.values():
+  # Print the number of rows being added
+  print("Adding {} rows".format(df.shape[0]))
+  # Append df to all_responses, assign result
+  all_responses = all_responses.append(df)
+
+# Graph employment statuses in sample
+counts = all_responses.groupby("EmploymentStatus").EmploymentStatus.count()
+counts.plot.barh()
+plt.show()
+
